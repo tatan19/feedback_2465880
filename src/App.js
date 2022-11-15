8dc27c2cfd0fd309855c6684ed5f4bde00d1f0ef
HarrisonIbarra <hibarra65@misena.edu.co>
2022-11-15 09:40:27 -0500

    React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const bootcamps = [
+        {
+            id: 1,
+            name: 'PHP Bootcamps',
+            average_rating: 8
+        },
+        {
+            id: 1,
+            name: 'JavaScript Bootcamps',
+            average_rating: 10
+        }
+    ]
+    return (
+        <>
+            <h1>Listado de Bootcamps</h1>
+            <ul>
+                {bootcamps.map((bootcamp, index) => (
+                    <li key={index}> {bootcamp.name} </li>
+                ))}
+            </ul>
+        </>
+    )
 }
 
-export default App;
+export default App
